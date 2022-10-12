d47fb887c645462cf8340cedf962116656828f5c
RAPHAEL UMALI <raph.umali@gmail.com>
2022-10-11 18:57:33 -0600

    Responsive design complete. TODO: Dark mode & host

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <div className="a-left">
         <div className="a-card bg"></div>
         <div className="a-card">
-          <img src={AboutMe} alt="lightbulb" className="a-img" />
+          <img src={AboutMe} alt="me" className="a-img" />
         </div>
       </div>
       <div className="a-right">
